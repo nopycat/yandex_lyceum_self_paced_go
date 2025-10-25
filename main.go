66535455107775cd21cd923dd5a601e8d1093b60
Slava <gubarenko2006@mail.ru>
2025-10-25 17:41:29 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"errors"
+	"unicode/utf8"
+)
+
+var ErrInvalidUTF8 = errors.New("invalid utf8")
+
+func GetUTFLength(input []byte) (int, error) {
+	if !utf8.Valid(input) {
+		return 0, ErrInvalidUTF8
+	}
+
+	return utf8.RuneCount(input), nil
+}
+
+func main() {
+
+}
