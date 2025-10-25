05085e8dff5c75e4c627e1ed94d41e512df7833f
Slava <gubarenko2006@mail.ru>
2025-10-25 19:17:37 +0300

    Описание изменений

--- main.go
+++ main.go
@@ -1,20 +1,59 @@
 package main
 
-import (
-	"errors"
-	"unicode/utf8"
-)
+import "fmt"
 
-var ErrInvalidUTF8 = errors.New("invalid utf8")
+type Animal interface {
+	MakeSound() string
+	GetName() string
+	GetInfo() string
+}
 
-func GetUTFLength(input []byte) (int, error) {
-	if !utf8.Valid(input) {
-		return 0, ErrInvalidUTF8
+type animal struct {
+	name    string
+	species string
+	age     int
+	sound   string
+}
+
+func (a animal) GetInfo() string {
+	return fmt.Sprintf("Имя: %s, Вид: %s, Возраст: %d",
+		a.name,
+		a.species,
+		a.age)
+}
+
+func (a animal) GetName() string {
+	return a.name
+}
+
+func (a animal) MakeSound() string {
+	return a.sound
+}
+
+func NewAnimal(name, species string, age int, sound string) Animal {
+	return animal{name: name, species: species, age: age, sound: sound}
+}
+
+func ZooShow(animals []Animal) {
+	for _, animal := range animals {
+		fmt.Println(animal.GetInfo())
+		fmt.Println(animal.MakeSound())
 	}
+}
 
-	return utf8.RuneCount(input), nil
+type ZooKeeper struct {
+	Animal
+}
+
+func (s ZooKeeper) Feed(animal Animal) {
+	fmt.Printf("Смотритель зоопарка кормит %s. %s!\n", animal.GetName(), animal.MakeSound())
 }
 
 func main() {
+	tiger := NewAnimal("Барсик", "Тигр", 5, "Ррр")
+	penguin := NewAnimal("Пиня", "Пингвин", 2, "Кря")
+	ZooShow([]Animal{tiger, penguin})
 
+	keeper := ZooKeeper{}
+	keeper.Feed(tiger)
 }
