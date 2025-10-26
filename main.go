4e2667a9780997793c3cb517ba95fa68e049c6f8
Slava <gubarenko2006@mail.ru>
2025-10-26 16:29:47 +0300

    Final

--- main.go
+++ main.go
@@ -1,5 +1,121 @@
 package main
 
+import (
+	"sort"
+)
+
+type MatchInterface interface {
+	goalsSort(players []Player) []Player
+	ratingSort(players []Player) []Player
+	gmSort(players []Player) []Player
+}
+
+func goalsSort(players []Player) []Player {
+	sort.Slice(players, func(i, j int) bool {
+		playerI := players[i]
+		playerJ := players[j]
+
+		goalI := playerI.Goals
+		goalJ := playerJ.Goals
+
+		if goalI != goalJ {
+			return goalI > goalJ
+		}
+
+		raitI := playerI.Rating
+		raitJ := playerJ.Rating
+
+		if raitI != raitJ {
+			return raitI > raitJ
+		}
+
+		return playerI.Name < playerJ.Name
+	})
+	return players
+}
+
+func ratingSort(players []Player) []Player {
+	sort.Slice(players, func(i, j int) bool {
+		playerI := players[i]
+		playerJ := players[j]
+
+		raitI := playerI.Rating
+		raitJ := playerJ.Rating
+
+		if raitI != raitJ {
+			return raitI > raitJ
+		}
+		return playerI.Name < playerJ.Name
+	})
+	return players
+}
+
+func gmSort(players []Player) []Player {
+	sort.Slice(players, func(i, j int) bool {
+		playerI := players[i]
+		playerJ := players[j]
+
+		goalI := playerI.Goals
+		goalJ := playerJ.Goals
+
+		missI := playerI.Misses
+		missJ := playerJ.Misses
+		var gm1, gm2 float64
+		if missI != 0 {
+			gm1 = float64(goalI) / float64(missI)
+		} else {
+			gm1 = float64(goalI) / 2
+		}
+
+		if missJ != 0 {
+			gm2 = float64(goalJ) / float64(missJ)
+		} else {
+			gm2 = float64(goalJ) / 2
+		}
+		if gm1 != gm2 {
+			return gm1 > gm2
+		}
+
+		raitI := playerI.Rating
+		raitJ := playerJ.Rating
+
+		if raitI != raitJ {
+			return raitI > raitJ
+		}
+		return playerI.Name < playerJ.Name
+	})
+	return players
+}
+
+type Player struct {
+	Name    string
+	Goals   int
+	Misses  int
+	Assists int
+	Rating  float64
+}
+
+func NewPlayer(name string, goals, misses, assists int) Player {
+	pl := Player{
+		Name:    name,
+		Goals:   goals,
+		Misses:  misses,
+		Assists: assists,
+	}
+	pl.calculateRating()
+	return pl
+}
+
+func (s *Player) calculateRating() {
+	numerator := float64(s.Goals) + float64(s.Assists)/2
+
+	if s.Misses != 0 {
+		s.Rating = numerator / float64(s.Misses)
+	} else {
+		s.Rating = numerator
+	}
+}
+
 func main() {
 
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestCalculateRating(t *testing.T) {
+	tests := []struct {
+		name           string
+		player         Player
+		expectedRating float64
+	}{
+		{
+			name:           "No misses",
+			player:         NewPlayer("Player1", 10, 0, 4),
+			expectedRating: 12.0,
+		},
+		{
+			name:           "Normal case",
+			player:         NewPlayer("Player2", 8, 2, 4),
+			expectedRating: 5.0,
+		},
+		{
+			name:           "Zero assists",
+			player:         NewPlayer("Player3", 5, 1, 0),
+			expectedRating: 5.0,
+		},
+		{
+			name:           "All stats",
+			player:         NewPlayer("Player4", 12, 3, 6),
+			expectedRating: 5.0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.player.Rating != tt.expectedRating {
+				t.Errorf("Expected rating %.2f, got %.2f", tt.expectedRating, tt.player.Rating)
+			}
+		})
+	}
+}
