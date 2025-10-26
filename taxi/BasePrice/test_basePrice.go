f845e031aebb07f65d38d41cc7affcf035c52828
Slava <gubarenko2006@mail.ru>
2025-10-26 16:57:19 +0300

    create model Base Price

---
+++ taxi/BasePrice/test_basePrice.go
@@ -0,0 +1,15 @@
+package basePrice
+
+import "testing"
+
+func TestCalculateBasePrice(t testing.T) {
+
+	trip1 := TripParameters{Distance: 8.5, Duration: 20}
+
+	got := CalculateBasePrice(trip1)
+	want := 125
+	if got != float64(want) {
+		t.Fail()
+	}
+
+}
