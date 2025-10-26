84db47e8b924437852ab741bab792655112553a1
Slava <gubarenko2006@mail.ru>
2025-10-26 17:15:23 +0300

    fix basePrice and create TimeCoefficients

--- taxi/BasePrice/basePrice.go
+++ taxi/BasePrice/basePrice.go
@@ -1,15 +1,16 @@
 package basePrice
 
 const (
-	pricePerKm     = 10.0
-	pricePerMinute = 2.0
+	pricePerKm     = 10.0 // Руб за км (в реальности это сложная формула)
+	pricePerMinute = 2.0  // Руб за минуту
 )
 
 type TripParameters struct {
-	Distance float64
-	Duration float64
+	Distance float64 // Расстояние в км (рассчитывается через Яндекс Карты)
+	Duration float64 // Время в минутах (прогнозируется с учётом истории поездок)
 }
 
 func CalculateBasePrice(tp TripParameters) float64 {
+	// Реализуем простую формулу: цена = расстояние * тариф + время * тариф
 	return tp.Distance*pricePerKm + tp.Duration*pricePerMinute
 }
