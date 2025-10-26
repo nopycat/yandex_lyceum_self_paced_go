52b115fed26d592e454e9d1d41776e78f84130cd
Slava <gubarenko2006@mail.ru>
2025-10-26 17:52:37 +0300

    create Traffic Congestion

---
+++ taxi/TrafficCongestion/trafficCongestion.go
@@ -0,0 +1,21 @@
+package trafficCongestion
+
+import (
+	"awesomeProject/taxi/weatherCoefficient"
+)
+
+func GetWeatherMultiplier(weather weatherCoefficient.WeatherData) float64 {
+	coef := 1.0
+	switch weather.Condition {
+	case weatherCoefficient.Rain:
+		coef *= 1.125
+	case weatherCoefficient.HeavyRain:
+		coef *= 1.2
+	case weatherCoefficient.Snow:
+		coef *= 1.15
+	}
+	if weather.WindSpeed > 15 {
+		coef += 0.1
+	}
+	return coef
+}
