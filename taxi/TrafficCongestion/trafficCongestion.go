0f3d258fd2ad9fa14327a66c26ceb146f5b1e0bf
Slava <gubarenko2006@mail.ru>
2025-10-27 16:44:08 +0300

    dded traffic jams

--- taxi/TrafficCongestion/trafficCongestion.go
+++ taxi/TrafficCongestion/trafficCongestion.go
@@ -19,3 +19,21 @@ func GetWeatherMultiplier(weather weatherCoefficient.WeatherData) float64 {
 	}
 	return coef
 }
+
+type TrafficClient interface {
+	GetTrafficLevel(lat, lng float64) int // 1–5
+}
+
+func GetTrafficMultiplier(trafficLevel int) float64 {
+	return 1.0 + float64(trafficLevel-1)*0.1
+}
+
+type PriceCalculator struct {
+	TrafficClient TrafficClient
+}
+
+type RealTrafficClient struct{}
+
+func (c *RealTrafficClient) GetTrafficLevel(lat, lng float64) int {
+	return 3 // Константное значение в нашем примере, в реальности оно будет вычисляться сервисом Яндекс Карты
+}
